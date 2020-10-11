58495a1978d64b1a51fd4a785e84b8a21dc6e603
Arezou Sohrabvand <200387480@student.georgianc.on.ca>
2020-10-11 14:18:01 -0400

    I added a new view called AddCategory with a razor view

---
+++ HomeAccessoriesStore/Controllers/FurnitureController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeAccessoriesStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeAccessoriesStore.Controllers
+{
+    public class FurnitureController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = new List<Category>();
+            for(var i = 1; i <= 10; i++)
+            {
+                categories.Add(new Category { Id = i, Name = "Category" + i.ToString() });
+             
+            }
+            return View(categories);
+        }
+        public IActionResult Details(string category)
+        {
+            ViewBag.category = category;
+            return View();
+        }
+        //Furniture /AddCategory
+        public IActionResult AddCategory()
+        {
+            //load a form to capture an obj from user
+            return View();
+        }
+    }
+}
